f90826e7a0b458c94a3869231478651bb39acb58
Alaa Wadi <84800142+alaawadi@users.noreply.github.com>
2022-10-13 13:00:19 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -157,8 +157,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'alaawadi98@gmail.com'
-EMAIL_HOST_PASSWORD = 'wsmwopkzcltrekty'
+EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_PASSWORD = '********'
 EMAIL_USE_TLS = True
 EMAIL_PORT = '587'
 
